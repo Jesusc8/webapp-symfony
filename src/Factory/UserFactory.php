0dd249175212b630b1d6d7beed042f436f0dd30d
Jesus Castro Java <jesusmcastror@gmail.com>
2025-11-17 13:48:08 +0100

    [feat] Add an automation of log with factory

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityRepository;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<User>
+ */
+final class UserFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(
+        private UserPasswordHasherInterface $passwordHasher
+        )
+    { }
+
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+        /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable    {
+        return [
+            'email' => self::faker()->email(),
+            'name' => self::faker()->name(),
+            'password' => 'password',
+            'roles' => [],
+        ];
+    }
+
+        /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            ->afterInstantiate(function(User $user): void {
+                $passwordHasher = $this->passwordHasher->hashPassword($user, 'password');
+
+                $user->setPassword($passwordHasher);
+            })
+        ;
+    }
+}
